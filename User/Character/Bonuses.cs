a8efaa710241a6da8bbf1a50176b4c756dcec624
jandar78 <juanmanuel@gonzalezmoreno.org>
2015-02-22 20:56:01 -0500

    New files that didn't get included in last commit.

---
+++ User/Character/Bonuses.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoUtils;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB;
+using CharacterEnums;
+
+namespace Character {
+    /// <summary>
+    /// This class can hold Bonus/Penalty for each type.  This way we can just offset bonuses and penalties at the core.
+    /// We can display to the user for example that their DEX is -5% or Dodge is +10% instead of doing Bonus Dodge = 15% Penalty Dodge = -5%
+    /// </summary>
+   public class StatBonuses {
+        
+        private Dictionary<string, Tuple<double, DateTime>> _bonus;
+        
+        /// <summary>
+        /// Adds an amount (positive or negative) to the type specified. Passing in zero or null for the time will make this bonus never expire.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="amount"></param>
+        /// <param name="time"></param>
+        public void Add(BonusTypes name, double amount, int time = 0) {
+            if (_bonus.ContainsKey(name.ToString())) {
+                _bonus[name.ToString()] = new Tuple<double, DateTime>(_bonus[name.ToString()].Item1 + amount, time == 0 ? DateTime.MaxValue : _bonus[name.ToString()].Item2.AddSeconds(time));
+            }
+            else {
+                _bonus.Add(name.ToString(), new Tuple<double, DateTime>(amount, time == 0 ? DateTime.MaxValue : DateTime.Now.AddSeconds(time)));
+            }
+        }
+
+        public void Remove(BonusTypes name) {
+            if (_bonus.ContainsKey(name.ToString())) {
+                _bonus.Remove(name.ToString());
+            }
+        }
+
+        public BsonArray GetBson() {
+            BsonArray bonuses = new BsonArray();
+
+            BsonDocument values = new BsonDocument(){
+                {"Name", ""},
+                {"Amount",""},
+                {"Time",""}
+            };
+
+            foreach (KeyValuePair<string, Tuple<double, DateTime>> item in _bonus) {
+                values["Name"] = item.Key;
+                values["Amount"] = (double)item.Value.Item1;
+                values["Time"] = (DateTime)item.Value.Item2;
+
+                bonuses.Add(values);
+            }
+                        
+            return bonuses;
+        }
+
+        public void LoadFromBson(BsonArray array) {
+            foreach (BsonDocument doc in array) {
+                this.Add((BonusTypes)Enum.Parse(typeof(BonusTypes), doc["Name"].AsString), doc["Amount"].AsDouble, doc["Time"].AsInt32);
+            }
+
+        }
+
+        public double GetBonus(BonusTypes type) {
+            double bonus = 0.0d;
+            if (_bonus.ContainsKey(type.ToString())) {
+                bonus = _bonus[type.ToString()].Item1;
+            }
+
+            return bonus;
+        }
+
+        /// <summary>
+        /// Calling this will remove any bonuses or penalties whose time has expired.
+        /// </summary>
+        /// <returns></returns>
+        public ClientHandling.Message Cleanup() {
+            List<string> messages = new List<string>();
+            MongoCollection bonusCollection = MongoUtils.MongoData.GetCollection("Messages", "Bonuses");
+            BsonDocument found = null;
+            BsonArray array = null;
+            IMongoQuery query = null;
+            ClientHandling.Message message = new ClientHandling.Message();
+            foreach (KeyValuePair<string, Tuple<double, DateTime>> item in _bonus) {
+                if (item.Value.Item2 != DateTime.MaxValue && DateTime.Now >= item.Value.Item2) {
+                    query = Query.EQ("_id", item.Key);
+                    found = bonusCollection.FindOneAs<BsonDocument>(query).AsBsonDocument;
+                    //let's add the messages that removing the bonus/penalty could have
+                    array = found["Messages"][0]["Self"].AsBsonArray;
+                    int choice = Extensions.RandomNumber.GetRandomNumber().NextNumber(0, array.Count());
+
+                    message.Self = array[choice].AsString;
+
+                    array = found["Messages"][0]["Target"].AsBsonArray;
+                    if (array.Count >= choice - 1) {
+                        message.Target = array[choice].AsString;
+                    }
+
+                    array = found["Messages"][0]["Others"].AsBsonArray;
+                    if (array.Count == choice - 1) {
+                        message.Room = array[choice].AsString;
+                    }
+
+                    //remove the bonus/penalty
+                    Remove((BonusTypes)Enum.Parse(typeof(BonusTypes),item.Key));
+                }
+            }
+
+            return message;
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<string, Tuple<double, DateTime>> item in _bonus) {
+                sb.AppendLine(string.Format("{0}: {1:p}",item.Key, item.Value.Item1));
+            }
+
+            return sb.ToString();
+        }
+        
+    }
+}
---
+++ WorldBuilder/Helper Forms/ModifiersForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WorldBuilder {
+    partial class ModifiersForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.amount = new System.Windows.Forms.Label();
+            this.amountValue = new System.Windows.Forms.TextBox();
+            this.attributeValue = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Attribute";
+            // 
+            // amount
+            // 
+            this.amount.AutoSize = true;
+            this.amount.Location = new System.Drawing.Point(26, 54);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(43, 13);
+            this.amount.TabIndex = 1;
+            this.amount.Text = "Amount";
+            // 
+            // amountValue
+            // 
+            this.amountValue.Location = new System.Drawing.Point(79, 51);
+            this.amountValue.Name = "amountValue";
+            this.amountValue.Size = new System.Drawing.Size(121, 20);
+            this.amountValue.TabIndex = 2;
+            this.amountValue.TextChanged += new System.EventHandler(this.amountValue_TextChanged);
+            // 
+            // attributeValue
+            // 
+            this.attributeValue.AutoCompleteCustomSource.AddRange(new string[] {
+            "HEALTH",
+            "ENDURANCE",
+            "STRENGTH",
+            "DEXTERITY",
+            "INTELLIGENCE",
+            "CHARISMA"});
+            this.attributeValue.FormattingEnabled = true;
+            this.attributeValue.Items.AddRange(new object[] {
+            "Hitpoints",
+            "Strength",
+            "Dexterity",
+            "Intelligence",
+            "Charisma",
+            "Endurance"});
+            this.attributeValue.Location = new System.Drawing.Point(79, 13);
+            this.attributeValue.Name = "attributeValue";
+            this.attributeValue.Size = new System.Drawing.Size(121, 21);
+            this.attributeValue.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(251, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // AffectedForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 239);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.attributeValue);
+            this.Controls.Add(this.amountValue);
+            this.Controls.Add(this.amount);
+            this.Controls.Add(this.label1);
+            this.Name = "AffectedForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "AffectedForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label amount;
+        private System.Windows.Forms.TextBox amountValue;
+        private System.Windows.Forms.ComboBox attributeValue;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
